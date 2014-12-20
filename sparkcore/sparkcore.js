2c35f3d7ad4932360783f80ea51dd7cf611a8325
Krishnaraj Varma <krvarma@gmail.com>
2014-12-20 23:07:09 +0530

    Multiple Spark Core issue fixed, input msg is handled properly
    Multiple Spark Core issue fixed, input msg is handled properly

--- sparkcore/sparkcore.js
+++ sparkcore/sparkcore.js
@@ -2,25 +2,16 @@ var EventSource = require('eventsource');
 var Request = require("request");
 var querystring = require('querystring');
 
-var selectedDevice = null;
-var sparkmodule = null;
-
 module.exports = function(RED) {
-	var core_id;
-	var access_token;
-	var param;
-	var name;
-	var repeat;
-	var interval_id;
-	var method;
-	var es;
-	var baseurl;
-
+	// Node-RED Input Module
     function SparkCoreIN(n) {
+		var sparkmodule = null;
+	
         RED.nodes.createNode(this,n);
 		
 		sparkmodule = this;
 		
+		// Get all properties
         this.interval_id = null;
 		this.repeat = n.repeat * 1000;
 		this.name = n.fve;
@@ -28,14 +19,17 @@ module.exports = function(RED) {
 		this.method = n.method;
 		this.baseurl = n.baseurl;
 		
+		// Check base URL or default to Spark Cloud URL.
 		if(this.baseurl === null || this.baseurl === ''){
 			this.baseurl = "https://api.spark.io";
 		}
 		
 		console.log("Using base URL: " + this.baseurl);
 		
+		// Get all credentials
 		var credentials = RED.nodes.getCredentials(n.id);
 		
+		// Check access token
 		if ((credentials) && (credentials.hasOwnProperty("accesstoken"))) { 
 			this.access_token = credentials.accesstoken; 
 		}
@@ -43,6 +37,7 @@ module.exports = function(RED) {
 			this.error("No Spark Core access token set"); 
 		}
         
+		// Check core id
 		if ((credentials) && (credentials.hasOwnProperty("coreid"))) { 
 			this.core_id = credentials.coreid; 
 		}
@@ -50,56 +45,120 @@ module.exports = function(RED) {
 			this.error("No Spark Core device id set"); 
 		}
 		
-		if(this.method == "function"){
-			if (this.repeat && !isNaN(this.repeat) && this.repeat > 0) {
-				this.log("Repeat = "+this.repeat);
-				
-				this.interval_id = setInterval( function() {
-					sparkmodule.emit("callfunction",{});
-				}, this.repeat );
-			} else{
-				setTimeout( function(){ sparkmodule.emit("callfunction",{}); }, 100);
-			}
-		}
-		else if(this.method == "subscribe"){
-			var url = this.baseurl + "/v1/devices/" + this.core_id + "/events/" + this.name + "?access_token=" + this.access_token;
-			
-			this.es = new EventSource(url);
-			
-			console.log(url);
+		setTimeout( function(){ sparkmodule.emit("process",{}); }, 100);
 		
-			this.es.addEventListener(this.name, function(e){
-				var data = JSON.parse(e.data);
+		// Called when there an input
+		this.on("input", function(msg){
+			// Retrieve all parameters from Message
+			var val = msg.name;
 			
-				var msg = {
-					raw: data,
-					payload:data.data,
-					published_at: data.published_at,
-					id: data.coreid
-				};
+			console.log(msg);
 			
-				sparkmodule.send(msg);
-			}, false);
+			// Retrieve name
+			if(val && val.length > 0){
+				this.name = val;
+			}
+			
+			val = msg.payload;
+			
+			// Retrieve payload
+			if(val && val.length > 0){
+				this.param = val;
+			}
+			
+			val = msg.operation;
+			
+			// Retrieve Operation
+			if(val && val.length > 0){
+				this.method = val;
+			}
+			
+			val = msg.baseurl;
+			
+			// Retrieve Base URL
+			if(val && val.length > 0){
+				this.baseurl = val;
+			}
+			
+			console.log("Operation: " + this.method);
+			console.log("Name: " + this.name);
+			console.log("Parameter: " + this.param);
+			
+			setTimeout( function(){ sparkmodule.emit("process",{}); }, 100);
+		});
+		
+		// Perform operations based on the method parameter.
+		this.on("process", function(){
+			// Function
+			if(this.method == "function"){
+				// Check for repeat and start timer
+				if (this.repeat && !isNaN(this.repeat) && this.repeat > 0) {
+					console.log("Repeat = "+this.repeat);
+					
+					this.interval_id = setInterval( function() {
+						sparkmodule.emit("callfunction",{});
+					}, this.repeat );
+				} 
+				// There is no repeat, just start once
+				else if (this.name && this.name.length > 0){
+					setTimeout( function(){ sparkmodule.emit("callfunction",{}); }, 100);
+				}
+			}
+			// Event Subscription
+			else if(this.method == "subscribe"){
+				var url = this.baseurl + "/v1/devices/" + this.core_id + "/events/" + this.name + "?access_token=" + this.access_token;
+				
+				this.es = new EventSource(url);
+				
+				console.log(url);
+			
+				// Add Event Listener
+				this.es.addEventListener(this.name, function(e){
+					var data = JSON.parse(e.data);
+				
+					var msg = {
+						raw: data,
+						payload:data.data,
+						published_at: data.published_at,
+						id: data.coreid
+					};
+				
+					sparkmodule.send(msg);
+				}, false);
 
-			this.es.onerror = function(){
-				console.log('ES Error');
-			};
-		}
-		else if(this.method == "variable"){
-			if (this.repeat && !isNaN(this.repeat) && this.repeat > 0) {
-				this.log("Repeat = "+this.repeat);
-				
-				this.interval_id = setInterval( function() {
-					sparkmodule.emit("getvariable",{});
-				}, this.repeat );
-			} else{
-				setTimeout( function(){ sparkmodule.emit("getvariable",{}); }, 100);
+				this.es.onerror = function(){
+					console.log('ES Error');
+				};
 			}
-		}
+			// Read variable
+			else if(this.method == "variable"){
+				// Check for repeat and start timer
+				if (this.repeat && !isNaN(this.repeat) && this.repeat > 0) {
+					console.log("Repeat = "+this.repeat);
+					
+					this.interval_id = setInterval( function() {
+						sparkmodule.emit("getvariable",{});
+					}, this.repeat );
+				} 
+				// There is no repeat, just start once
+				else if (this.name && this.name.length > 0){
+					setTimeout( function(){ sparkmodule.emit("getvariable",{}); }, 100);
+				}
+			}
+		});
 		
+		// Call Spark Core function
 		this.on("callfunction", function(){
 			var url = this.baseurl + "/v1/devices/" + this.core_id + "/" + this.name;
 			
+			console.log("Calling function...");
+			
+			console.log("URL: " + this.baseurl);
+			console.log("Core ID: " + this.core_id);
+			console.log("Function Name: " + this.name);
+			console.log("Parameter: " + this.param);
+			
+			// Call Spark Core function
 			Request.post(
 				url, 
 				{
@@ -110,6 +169,7 @@ module.exports = function(RED) {
 				},
 				function (error, response, body){
 					console.log(body);
+					// If not error then prepare message and send
 					if(!error && response.statusCode == 200){
 						var data = JSON.parse(body);
 						var msg = {
@@ -126,12 +186,19 @@ module.exports = function(RED) {
 			);
 		});
 		
+		// Read Spark Core variable
 		this.on("getvariable", function(){
 			var url = this.baseurl + "/v1/devices/" + this.core_id + "/" + this.name + "?access_token=" + this.access_token;
 			
+			console.log("Reading variable '" + this.name + "'");
+			console.log("URL '" + url + "'");
+			
+			// Read Spark Core variable
 			Request.get(url,
 				function (error, response, body){
 					console.log(body);
+					
+					// If not error then prepare message and send
 					if(!error && response.statusCode == 200){
 						var data = JSON.parse(body);
 						
@@ -148,7 +215,10 @@ module.exports = function(RED) {
 		});
     }
 	
+	// Node-RED Output variable
 	function SparkCoreOUT(n) {
+		var sparkmodule;
+		
         RED.nodes.createNode(this,n);
 		
 		sparkmodule = this;
@@ -157,14 +227,17 @@ module.exports = function(RED) {
 		this.param = n.param;
 		this.baseurl = n.baseurl;
 		
+		// Check base URL or default to Spark Cloud URL.
 		if(this.baseurl === null || this.baseurl === ''){
 			this.baseurl = "https://api.spark.io";
 		}
 		
 		console.log("Using base URL: " + this.baseurl);
 		
+		// Get all credentials
 		var credentials = RED.nodes.getCredentials(n.id);
 		
+		// Check access token
 		if ((credentials) && (credentials.hasOwnProperty("accesstoken"))) { 
 			this.access_token = credentials.accesstoken; 
 		}
@@ -172,6 +245,7 @@ module.exports = function(RED) {
 			this.error("No Spark Core access token set"); 
 		}
         
+		// Check Core ID
 		if ((credentials) && (credentials.hasOwnProperty("coreid"))) { 
 			this.core_id = credentials.coreid; 
 		}
@@ -180,27 +254,59 @@ module.exports = function(RED) {
 		}
 		
 		this.on("input", function(msg){
-			var url = this.baseurl + "/v1/devices/" + this.core_id + "/" + this.name;
+			var val = msg.name;
 			
+			console.log(msg);
+			
+			// Retrieve name
+			if(val && val.length > 0){
+				this.name = val;
+			}
+			
+			val = msg.payload;
+			
+			// Retrieve payload
+			if(val && val.length > 0){
+				this.param = val;
+			}
+			
+			val = msg.baseurl;
+			
+			// Retrieve Base URL
+			if(val && val.length > 0){
+				this.baseurl = val;
+			}
+			
+			var url = this.baseurl + "/v1/devices/" + this.core_id + "/" + this.name;
 			var parameter = this.param;
 			
 			if(parameter.length == 0){
 				parameter = msg.payload;
 			}
 			
+			// Prepare Post Data
 			var postdata = {
 				form: {
 					access_token: this.access_token,
 					args: parameter
 				}
 			};
+			
+			console.log("[OUT]: Calling function...");
+			console.log("[OUT]: " + this.core_id);
+			console.log("[OUT]: " + this.name);
 				
+			// Call Spark Core function
 			Request.post(
 				url, 
 				postdata,
 				function (error, response, body){
+					// If not error, send to Node-RED
 					if(!error && response.statusCode == 200){
 						var data = JSON.parse(body);
+						
+						console.log(data);
+						
 						var msg = {
 							raw: data,
 							payload: data.return_value,
